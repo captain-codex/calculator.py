dd1f779caab3d14a62608e3bf7eb311eae85422b
captain-codex <princetonckc.havardbrain@gmail.com>
2024-09-17 16:39:15 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("enter first number: " ))
+num2 = float(input("enter second number: "))
+num3 = float(input("enter third number: "))
+print("sum", num1 + num2)
+print("subtraction" , num1 - num2)
+print("division", num1 / num2)
+print("multiplication", num1 * num2)
+print("exponential",num1 ** num2)
+import math
+num = float(input("enter number to calculate square root; "))
+sqrt_result = math.sqrt(num)
+print("result of square root:", sqrt_result)
+import math
+print(math.factorial(5))
+
+
+
+
